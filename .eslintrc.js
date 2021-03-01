b16ea15fb36d4ab23ba19ef78b02dfbdc75fd12d
Dompou06 <30805395+Dompou06@users.noreply.github.com>
2021-03-01 17:48:58 +0100

    Initialisation server nodejs, git flow esLint et Jest

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+/* eslint-disable indent */
+module.exports = {
+extends: 'standard',
+overrides: [
+    {
+files: [
+        '**/*.test.js'
+],
+env: {
+        jest: true // now **/*.test.js files' env has both es6 *and* jest
+},
+plugins: ['jest'],
+rules: {
+'jest/no-disabled-tests': 'warn',
+'jest/no-focused-tests': 'error',
+'jest/no-identical-title': 'error',
+        'jest/prefer-to-have-length': 'warn',
+        'jest/valid-expect': 'error'
+}
+    }
+]
+}
